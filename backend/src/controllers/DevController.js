188b014a82fa739615601a5e7d91ebdc195e5ad2
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2020-02-03 17:40:14 -0300

    Finishing backend - week10

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(req, res) {
@@ -36,9 +37,16 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      // Filtrar as conexões que estão a no maximo 
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      );
+      
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
-    console.log(dev);
     return res.json(dev);
   },
 
